2e1283a00e2f2c9c3d314cfd3252cfee388eaeaf
Edward Mario <emario@tyro.com>
2020-05-31 23:03:32 +1000

    fixes github login issue

--- centos-ansible.sh
+++ centos-ansible.sh
@@ -5,7 +5,10 @@ yum install -y wget
 useradd -m emario && usermod -aG wheel emario
 echo -e "changeme\nchangeme" | passwd emario
 mkdir -p /home/emario/.ssh && chmod 700 /home/emario && chown -R emario:emario /home/emario
-wget -P /tmp http://192.168.1.30:8000/authorized_keys
-mv /tmp/authorized_keys /home/emario/.ssh/authorized_keys && chmod 400 /home/emario/.ssh/authorized_keys && chown -R emario:emario /home/emario/.ssh/authorized_keys
+mkdir /tmp/ssh
+wget -P /tmp/ssh http://192.168.1.30:8000/authorized_keys
+wget -P /tmp/ssh http://192.168.1.30:8000/config/config
+wget -P /tmp/ssh http://192.168.1.30:8000/key/emario_private
+mv /tmp/ssh/* /home/emario/.ssh && chmod -R 700 /home/emario/.ssh && chown -R emario:emario /home/emario/.ssh
 mv /tmp/sshd_config /etc/ssh/sshd_config && chmod 644 /etc/ssh/sshd_config && chown -R root:root /etc/ssh/sshd_config
 service sshd start
